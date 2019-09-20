ab0a539744ab1ffb563b6b7a68ec0669c17fc9a9
Gia-Minh Doan <benoitgmd@gmail.com>
2019-09-20 15:46:56 -0400

    checkout button goes to checkout from menu

--- app/src/main/java/com/example/doanshop/MenuActivity.java
+++ app/src/main/java/com/example/doanshop/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.doanshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void openCheckout(View view) {
+        Intent intent1 = new Intent(this, CheckoutActivity.class);
+        startActivity(intent1);
     }
 }
