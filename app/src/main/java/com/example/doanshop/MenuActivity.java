362c7337948f40377eeca5bcb6f7293ef98e5f3f
Gia-Minh Doan <benoitgmd@gmail.com>
2019-09-20 20:22:00 -0400

    all add and remove button work as intended

--- app/src/main/java/com/example/doanshop/MenuActivity.java
+++ app/src/main/java/com/example/doanshop/MenuActivity.java
@@ -4,17 +4,91 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private int mAddm3 = 0;
+    public TextView mAddCount;
+
+    private int mAddms = 0;
+    public TextView mAddCountS;
+
+    private int mAddmx = 0;
+    public TextView mAddCountX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        mAddCount = (TextView) findViewById(R.id.subtotal_countm3);
+        mAddCountS = (TextView) findViewById(R.id.subtotal_countms);
+        mAddCountX = (TextView) findViewById(R.id.subtotal_countmx);
     }
 
     public void openCheckout(View view) {
         Intent intent1 = new Intent(this, CheckoutActivity.class);
         startActivity(intent1);
     }
+
+    public void add_model3(View view){
+         mAddm3++;
+        if (mAddCount != null){
+            mAddCount.setText(Integer.toString(mAddm3));
+        }
+    }
+
+    public  void remove_model3(View view){
+
+            if(mAddm3 <= 0){
+                mAddm3 = 0;
+                Toast.makeText(this, "Can't remove anymore.", Toast.LENGTH_SHORT).show();
+            }else {
+                mAddm3--;
+                mAddCount.setText(Integer.toString(mAddm3));
+            }
+
+    }
+
+
+    public void add_models(View view){
+        mAddms++;
+        if (mAddCountS != null){
+            mAddCountS.setText(Integer.toString(mAddms));
+        }
+    }
+
+    public  void remove_models(View view){
+
+        if(mAddms <= 0){
+            mAddms = 0;
+            Toast.makeText(this, "Can't remove anymore.", Toast.LENGTH_SHORT).show();
+        }else {
+            mAddms--;
+            mAddCountS.setText(Integer.toString(mAddms));
+        }
+
+    }
+
+    public void add_modelx(View view){
+        mAddmx++;
+        if (mAddCountX != null){
+            mAddCountX.setText(Integer.toString(mAddmx));
+        }
+    }
+
+    public  void remove_modelx(View view){
+
+        if(mAddmx <= 0){
+            mAddmx = 0;
+            Toast.makeText(this, "Can't remove anymore.", Toast.LENGTH_SHORT).show();
+        }else {
+            mAddmx--;
+            mAddCountX.setText(Integer.toString(mAddmx));
+        }
+
+    }
+
+
 }
